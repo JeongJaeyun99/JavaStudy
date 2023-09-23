670f6b1fff4c5441f6f92474c5152f25357b2258
jaeyun <wodbs0723@ut.ac.kr>
2023-09-23 22:15:47 +0900

    9/23 저녁

---
+++ Practice/src/test/Exam_10926.java
@@ -0,0 +1,14 @@
+package test;
+
+import java.util.Scanner;
+
+public class Exam_10926 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		String id = sc.next();
+		System.out.println(id + "??!");
+	}
+
+}
