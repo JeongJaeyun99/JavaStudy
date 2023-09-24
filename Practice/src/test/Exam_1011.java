81a597b09af0a0a91680a836e40ad6cc93930432
jaeyun <wodbs0723@ut.ac.kr>
2023-09-24 18:18:07 +0900

    9/24 저녁 커밋

--- Practice/src/test/Exam_1011.java
+++ Practice/src/test/Exam_1011.java
@@ -1,10 +1,26 @@
 package test;
 
+import java.util.Scanner;
+
 public class Exam_1011 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int cnt = 1;
+		int t = sc.nextInt();
+		for (int i = 0; i < t; i++) {
+				int x = sc.nextInt();
+				int y = sc.nextInt();
+				
+				int temp = (y-x) / 2;
+				
+					
+				}
+				
+				System.out.println(cnt);
+			}
+		}
 		
 	}
 
-}
