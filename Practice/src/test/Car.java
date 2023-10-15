c31b9ebfda181ad53ec27d796206ce22748d1f6c
jaeyun <wodbs0723@ut.ac.kr>
2023-10-15 20:22:25 +0900

    10/15 저녁

---
+++ Practice/src/test/Car.java
@@ -0,0 +1,22 @@
+package test;
+
+public class Car {
+	String model_name;
+	String engine;
+	int Engine_displace;
+	String fuel;
+	double fuel_effecient;
+	
+	public void startUp() {
+		
+	}
+	public void backUp() {
+		
+	}
+	public void speedUp() {
+		
+	}
+	public void speedDown() {
+		
+	}
+}
