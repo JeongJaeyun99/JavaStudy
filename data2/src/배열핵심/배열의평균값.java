d684b937e0934dd6a2a63bf7660b80160cd9965e
jaeyun <wodbs0723@ut.ac.kr>
2023-11-21 17:22:50 +0900

    11/21 저녁

---
+++ data2/src/배열핵심/배열의평균값.java
@@ -0,0 +1,7 @@
+package 배열핵심;
+
+public class 배열의평균값 {
+	public static void main(String[] args) {
+		System.out.println(String.valueOf(2));
+	}
+}
---
+++ data2/src/프로그래머스입문/T.java
@@ -0,0 +1,9 @@
+package 프로그래머스입문;
+
+public class T {
+
+	public static void main(String[] args) {
+		System.out.println(2%4);
+	}
+
+}
