c8cc4b982dd3ff1cd91779d00a47b3048d3b700a
jaeyun <wodbs0723@ut.ac.kr>
2023-12-10 22:23:55 +0900

    12/10 프로그래머스

--- programmers/src/level1/숫자_문자열과_영단어.java
+++ programmers/src/level1/숫자_문자열과_영단어.java
@@ -1,19 +1,18 @@
 package level1;
 
+import java.util.ArrayList;
+
 public class 숫자_문자열과_영단어 {
 
 	public static void main(String[] args) {
-		String a = "one";
-		String s = "";
-		switch (a) {
-		case "one": 
-			s += "1";
-			a = "";
-			break;
-
-		default:
-			break;
-		}
+		ArrayList<String> list = new ArrayList<String>();
+		list.add("s1");
+		list.add("s2");
+		list.add("s3");
+		list.add("s4");
+		System.out.println(list.contains("s1"));
+		String s = "1";
+		
 	}
 
 }
